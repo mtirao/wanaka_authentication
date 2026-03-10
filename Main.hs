ce311c80b4fe0074fb54ff44a5eee88641db9569
Marcos Tirao <marcos.tirao@icloud.com>
2026-03-10 20:54:13 +0100

    Start using  connection pool

--- Main.hs
+++ Main.hs
@@ -6,8 +6,9 @@ import Data.Text (pack)
 import Data.Text.Encoding (encodeUtf8)
 import qualified Data.Configurator as C
 import qualified Data.Configurator.Types as CT
-import qualified Hasql.Connection as S
-import qualified Hasql.Pool as P
+import Hasql.Connection
+import Hasql.Pool as P
+import Data.Time (NominalDiffTime)
 
 data DbConfig = DbConfig
     { dbName     :: String
@@ -37,18 +38,12 @@ main = do
     case dbConf of
         Nothing -> putStrLn "Error loading configuration"
         Just conf -> do
-            let connSettings = S.settings (encodeUtf8 $ pack $ dbHost conf)
+            let connSettings = settings (encodeUtf8 $ pack $ dbHost conf)
                                         (fromIntegral $ dbPort conf)
                                         (encodeUtf8 $ pack $ dbUser conf)
                                         (encodeUtf8 $ pack $ dbPassword conf)
                                         (encodeUtf8 $ pack $ dbName conf)
-            result <- S.acquire connSettings
-            case result of
-                Left err -> putStrLn $ "Error acquiring connection: " ++ show err
-                Right _ -> do
-                    putStrLn "Database connection established"
-                    putStrLn "Starting Servant server on port 3001 "
-                    -- TODO: Integrate proper connection pooling with hasql-pool
-                    -- For now, using single connection - pooling to be implemented
-                    run 3001 (app (error "TODO: Pool not yet integrated"))
+            pool <- P.acquire 10 60 60 60 connSettings
+            putStrLn "Starting Servant server on port 3001 "
+            run 3001 (app pool)
 
